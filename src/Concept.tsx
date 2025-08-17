06675fec74e19ad292d19148b487b63afc295ce8
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-17 12:03:50 +0530

    Explain the state of a component and useState

--- src/Concept.tsx
+++ src/Concept.tsx
@@ -1,18 +1,19 @@
+import { useState } from "react";
+
 export const Concept = () =>{
     let num = 0;
+    const [numValue,setNumValue] = useState(num)
 
     const handleMax = () =>{
-       num++
-       console.log("Num is: ",num)
+      setNumValue( prev => prev + 1)
     }
     const handleMin = () =>{
-        num--
-        console.log("Num is: ",num)
+        setNumValue( prev => prev - 1)
     }
 
 return(
     <>
-    <h1 style={{marginLeft: '40px'}}>{num}</h1>
+    <h1 style={{marginLeft: '40px'}}>{numValue}</h1>
     <button onClick={handleMax}>Max +</button>
     <button onClick={handleMin}>Min -</button>
     </>
