32d7cb8921ee664409138c97e19d550d3933413e
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-17 13:17:05 +0530

    Add react Bootsrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Test } from "./Test";
 import { Concept } from "./Concept";
+import { CourseMaterial } from "./components/CourseMaterial";
 
 function App() {
   return (
     <>
-      {/* <h1>Hello CMJD 111</h1>
-      <h1>Hello CMJD 111</h1>
-      <Test/> */}
-
-      <Concept/>
+    <CourseMaterial/>
     </>
   );
 }
